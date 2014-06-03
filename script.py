078fc71d9f558c26de6fd32a245acb58ed599be8
bobvoorneveld <bob.voorneveld@voipgrid.nl>
2014-06-03 11:45:32 +0200

    first commit

---
+++ script.py
@@ -0,0 +1,49 @@
+import json
+import os
+import re
+import unicodecsv
+
+
+home_dir = os.path.dirname(os.path.abspath(__file__))
+
+user_file = open(os.path.join(home_dir, 'users.json'))
+user_data = json.load(user_file)
+user_file.close()
+
+users = {}
+for user in user_data:
+    users.update({
+        user['id']: user['name'],
+    })
+
+
+def replace_username(matchobj):
+    return '@%s' % users[matchobj.group(1)]
+
+with open('import.csv', 'wb') as csvfile:
+    writer = unicodecsv.writer(csvfile)
+
+    for (dirpath, dirnames, filenames) in os.walk(home_dir):
+        for filename in filenames:
+            base_name, extension = os.path.splitext(filename)
+            if extension == '.json' and base_name != 'users':
+                json_file = open(os.path.join(dirpath, filename))
+                message_data = json.load(json_file)
+                json_file.close()
+
+                for message in message_data:
+                    # ts
+                    ts = message['ts'].split('.')[0]
+                    # channel
+                    channel = os.path.split(dirpath)[1]
+                    # username
+                    try:
+                        user = users[message['user']]
+                    except KeyError:
+                        continue
+                    if message.get('subtype', None):
+                        continue
+                    # text
+                    text = message['text']
+                    text = re.sub(r'<@(?P<username>.{9})>', replace_username, text)
+                    writer.writerow([ts, channel, user, text])
